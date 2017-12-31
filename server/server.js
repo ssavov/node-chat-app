5005421413a0a36860d1b8a544515269d9a91c3d
Svetlio <svetlio@netreact.com>
2017-12-31 03:46:57 +0200

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'svetlio',
+        text: 'hey',
+        createdAt: new Date()
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message)
+    });
+
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     })

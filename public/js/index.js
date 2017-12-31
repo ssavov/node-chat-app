768be0beac2e2deeffa3c6c7772bce76124a7ee5
Svetlio <svetlio@netreact.com>
2017-12-31 03:56:58 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Svet',
-        text: 'Yeeeeah'
-    });
 });
 
 socket.on('disconnect', function () {
